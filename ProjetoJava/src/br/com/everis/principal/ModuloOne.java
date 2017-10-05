7cbbfa4a90f11326f2251f1bcb8f08b1b7e38732
evbudi <everisbrasiludi@gmail.com>
2017-10-05 18:05:48 -0300

    [Versao 1]

---
+++ ProjetoJava/src/br/com/everis/principal/ModuloOne.java
@@ -0,0 +1,15 @@
+package br.com.everis.principal;
+
+public class ModuloOne {
+	
+	private static final String MSG = "Modulo Um Roudou";
+	
+
+	public String executa(){
+		
+		return MSG;
+		
+	}
+
+	
+}
