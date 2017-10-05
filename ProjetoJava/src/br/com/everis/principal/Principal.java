30724d7a6f5a5025454c1147104fbf6f76ecbf3c
evbudi <everisbrasiludi@gmail.com>
2017-10-05 17:59:02 -0300

    [Versao Zero]

---
+++ ProjetoJava/src/br/com/everis/principal/Principal.java
@@ -0,0 +1,13 @@
+package br.com.everis.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("#Incio Principal#");
+
+		
+		System.out.println("#Fim Principal#");
+	}
+
+}
